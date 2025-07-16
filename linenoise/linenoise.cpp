893cbf27aaee3eaa12a3ed8d9ab4813cd53e21ce
jgmdev <jgmdev@gmail.com>
2025-07-16 12:24:43 -0400

    Fix a segmentation fault on linenoise-ng wrong copy at setMaxHistory

--- linenoise/linenoise.cpp
+++ linenoise/linenoise.cpp
@@ -430,9 +430,9 @@ static int write32(int fd, char32_t* text32, int len32) {
 
 class Utf32String {
  public:
-  Utf32String() : _length(0), _data(nullptr) { 
+  Utf32String() : _length(0), _data(nullptr) {
     // note: parens intentional, _data must be properly initialized
-    _data = new char32_t[1](); 
+    _data = new char32_t[1]();
   }
 
   explicit Utf32String(const char* src) : _length(0), _data(nullptr) {
@@ -464,9 +464,9 @@ class Utf32String {
     memcpy(_data, src, len * sizeof(char32_t));
   }
 
-  explicit Utf32String(int len) : _length(0), _data(nullptr) { 
+  explicit Utf32String(int len) : _length(0), _data(nullptr) {
     // note: parens intentional, _data must be properly initialized
-    _data = new char32_t[len](); 
+    _data = new char32_t[len]();
   }
 
   explicit Utf32String(const Utf32String& that) : _length(that._length), _data(nullptr) {
@@ -2542,7 +2542,7 @@ int InputBuffer::getInputLine(PromptBase& pi) {
     if (terminatingKeystroke == -1) {
       c = linenoiseReadChar();  // get a new keystroke
 
-      keyType = 0; 
+      keyType = 0;
       if (c != 0) {
         // set flag that we got some input
         if (c == ctrlChar('C')) {
@@ -3315,8 +3315,7 @@ int linenoiseHistorySetMaxLen(int len) {
     if (len < tocopy) {
       tocopy = len;
     }
-    memcpy(newHistory, history + historyMaxLen - tocopy,
-           sizeof(char8_t*) * tocopy);
+    memcpy(newHistory, history, sizeof(char8_t*) * tocopy);
     free(history);
     history = newHistory;
   }
